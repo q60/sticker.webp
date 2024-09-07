7efda8811cc86d42ef6547c58955ebc0e5dd83b1
Kira Malinova <llathasa@outlook.com>
2024-09-07 22:37:21 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "sticker.webp";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) stdenv;
+    in {
+      packages = {
+        default = stdenv.mkDerivation rec {
+          name = "sticker.webp";
+          pname = name;
+
+          src = ./.;
+
+          dontBuild = true;
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 -t $out/bin sticker.webp
+            runHook postInstall
+          '';
+        };
+      };
+
+      apps.default = utils.lib.mkApp {drv = self.packages.${system}.default;};
+    });
+}
